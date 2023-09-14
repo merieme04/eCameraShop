aaadd473ede6437fc77248ff9431ac44f13e966b
merieme anasri <meriemeanasri04@gmail.com>
2023-09-14 14:05:11 +0100

    c

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import companyOne from "./images/company-1.webp";
+import companyTwo from "./images/company-2.webp";
+import companyThree from "./images/company-3.webp";
+import companyFour from "./images/company-4.webp";
+import companyFive from "./images/company-5.png";
+import companySix from "./images/company-6.webp";
+import bannerOne from "./images/banner-1.webp";
+import bannerTwo from "./images/banner-2.webp";
+import bannerThree from "./images/banner-3.webp";
+import logo from "./images/logo.png";
+
+export {
+  companyFive,
+  companyFour,
+  companyOne,
+  companyThree,
+  companyTwo,
+  companySix,
+  bannerOne,
+  bannerThree,
+  bannerTwo,
+  logo,
+};
